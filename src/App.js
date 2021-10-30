ce41ed14ef00189825c7d6bc214f034dee92aa0b
jv88899 <jv88899@gmail.com>
2021-10-30 07:36:33 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
+import Header from "components/Header";
+import CreatePost from "pages/CreatePost";
+import Home from "pages/Home";
+import Login from "pages/Login";
+import Signup from "pages/Signup";
+import { Route, Switch } from "react-router-dom";
+
 export default function App() {
-  return <>app</>;
+  return (
+    <>
+      <Route component={Header} />
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/createpost" component={CreatePost} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+import AppProviders from "AppProviders";
 import ReactDOM from "react-dom";
 import "styles/index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <>
+  <AppProviders>
     <App />
-  </>,
+  </AppProviders>,
   document.getElementById("root")
 );
 
