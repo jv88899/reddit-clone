d3fcf9e1065fa893dc5bd8dae34a42accc1e5ee1
jv88899 <jv88899@gmail.com>
2021-10-30 10:47:55 -0500

    created sign up form

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,3 +1,29 @@
+import Form from "components/shared/form/Form";
+import Input from "components/shared/form/Input";
+import InputWrapper from "components/shared/form/InputWrapper";
+import Label from "components/shared/form/Label";
+import SubmitButton from "components/shared/form/SubmitButton";
+
 export default function Signup() {
-  return <>signup</>;
+  return (
+    <Form>
+      <InputWrapper>
+        <Label>username</Label>
+        <Input type="text" />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>email</Label>
+        <Input type="email" />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>password</Label>
+        <Input type="password" />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>confirm password</Label>
+        <Input type="password" />
+      </InputWrapper>
+      <SubmitButton type="submit">sign up</SubmitButton>
+    </Form>
+  );
 }
