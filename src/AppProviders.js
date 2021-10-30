1854ef82380128ad96390b9c49287cfb561b330e
jv88899 <jv88899@gmail.com>
2021-10-30 07:47:21 -0500

    set up ThemeProvider in AppProviders.js

--- src/AppProviders.js
+++ src/AppProviders.js
@@ -1,8 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import { ThemeProvider } from "styled-components";
+import theme from "theme";
 
 const history = createBrowserHistory();
 
 export default function AppProviders({ children }) {
-  return <BrowserRouter history={history}>{children}</BrowserRouter>;
+  return (
+    <ThemeProvider theme={theme(true)}>
+      <BrowserRouter history={history}>{children}</BrowserRouter>
+    </ThemeProvider>
+  );
 }
