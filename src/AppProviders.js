ebaeadd9ff500039b0bb2a0949e0264060fb8108
jv88899 <jv88899@gmail.com>
2021-10-30 07:39:31 -0500

    create history prop for BrowserRouter in AppProviders.js

--- src/AppProviders.js
+++ src/AppProviders.js
@@ -1,5 +1,8 @@
 import { BrowserRouter } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 export default function AppProviders({ children }) {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return <BrowserRouter history={history}>{children}</BrowserRouter>;
 }
