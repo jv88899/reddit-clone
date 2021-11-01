4e856e19bad2693cb1a99f7cdd47bda90e6df540
jv88899 <jv88899@gmail.com>
2021-11-01 13:32:23 -0500

    set up react query in AppProviders.js

--- src/AppProviders.js
+++ src/AppProviders.js
@@ -2,13 +2,17 @@ import { BrowserRouter } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import { ThemeProvider } from "styled-components";
 import theme from "theme";
+import { QueryClient, QueryClient, QueryClientProvider } from "react-query";
 
+const QueryClient = new QueryClient();
 const history = createBrowserHistory();
 
 export default function AppProviders({ children }) {
   return (
-    <ThemeProvider theme={theme(true)}>
-      <BrowserRouter history={history}>{children}</BrowserRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme(true)}>
+        <BrowserRouter history={history}>{children}</BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
