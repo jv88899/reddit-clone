b4fcf80e10fdbb8e9df885e46c3704d33bad0ae3
jv88899 <jv88899@gmail.com>
2021-11-15 14:45:18 -0600

    added functionality to update view count

--- src/lib/hooks.js
+++ src/lib/hooks.js
@@ -1,1 +1,10 @@
-export function usePostViewCount() {}
+import { addView } from "lib/firebase";
+import { useEffect } from "react";
+
+export function usePostViewCount(postId) {
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      addView(postId);
+    }
+  }, [postId]);
+}
